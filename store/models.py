d1a5871e926e8ce657f077c1646c7d8af0411958
bibek-beebayk <beebayk0001@gmail.com>
2021-11-28 21:11:48 +0545

    Defining many to many relationship

--- store/models.py
+++ store/models.py
@@ -3,6 +3,10 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 
+class Promotion(models.Model):
+    decsription = models.CharField(max_length=255)
+    discount = models.FloatField()
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
@@ -14,6 +18,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
