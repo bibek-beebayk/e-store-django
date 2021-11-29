19f05d591b4d66e044ec4cd34592ab4c9b8bca8b
bibek-beebayk <beebayk0001@gmail.com>
2021-11-29 07:58:46 +0545

    Migrations

--- store/models.py
+++ store/models.py
@@ -14,8 +14,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -60,6 +61,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length = 255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
 class Cart(models.Model):
