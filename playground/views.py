2a35144d362245171075680e0a8dba50aab96882
bibek-beebayk <beebayk0001@gmail.com>
2021-11-30 20:40:05 +0545

    ORM 1

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
+from store.models import Product, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'hello.html')
+    query_set = Product.objects.filter(id__in= OrderItem.objects.values('product__id').distinct()).order_by('title')
+    return render(request, 'hello.html',{'products': list(query_set)})
